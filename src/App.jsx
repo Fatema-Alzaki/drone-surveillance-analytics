e579e1e77cc399dab6899ed6c0277aeea2961099
Fatema-Alzaki <fatimaalzaki242@gmail.com>
2026-03-07 01:24:31 +0300

    Initial React app setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Drone Surveillance Analytics</h1>
+      <p>Final Year Project Dashboard</p>
+    </div>
+  );
+}
+
+export default App;
